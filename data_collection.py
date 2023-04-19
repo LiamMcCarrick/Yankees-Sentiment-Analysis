a1f266b536262c989c2541a5516ad9634698d091
Liam McCarrick <wrmccarrick@gmail.com>
2023-04-19 01:22:56 -0400

    changed search limit

--- data_collection.py
+++ data_collection.py
@@ -5,7 +5,7 @@ from sentiment_analysis import sentiment_model
 # define parameters
 adv_search = '"yankees" "volpe" OR "donaldson" OR "judge" OR "falefa" OR "ikf" OR "hicks" lang:en until:2023-04-18 since:2023-04-04'
 test_search = '"yankees" "hicks" lang:en until:2023-04-18 since:2023-04-04'
-max_search = 50000
+max_search = 45000
 test_max = 10
 
 tweet_collection = sentiment_model(test_search, test_max)
