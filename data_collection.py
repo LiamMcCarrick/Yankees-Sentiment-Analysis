0fb9e9b2ed371682e8e9253b50989adf0778b117
Liam McCarrick <wrmccarrick@gmail.com>
2023-04-19 14:35:23 -0400

    exported csv files for each player

--- data_collection.py
+++ data_collection.py
@@ -3,13 +3,25 @@ import pandas as pd
 from sentiment_analysis import sentiment_model
 
 # define parameters
-adv_search = '"yankees" "volpe" OR "donaldson" OR "judge" OR "falefa" OR "ikf" OR "hicks" lang:en until:2023-04-18 since:2023-04-04'
-test_search = '"yankees" "hicks" lang:en until:2023-04-18 since:2023-04-04'
+juddge_search = '"yankees" "judge" lang:en until:2023-04-18 since:2023-03-30'
+volpe_search = '"yankees" "volpe" lang:en until:2023-04-18 since:2023-03-30'
+donaldson_search = '"yankees" "donaldson" lang:en until:2023-04-18 since:2023-03-30'
+ikf_search = '"yankees" "falefa" OR "ikf" lang:en until:2023-04-18 since:2023-03-30'
+hicks_search = '"yankees" "hicks" lang:en until:2023-04-18 since:2023-03-30'
 max_search = 45000
-test_max = 10
 
-tweet_collection = sentiment_model(test_search, test_max)
+# export each player tweets to csv
+tweet_judge = sentiment_model(juddge_search, max_search)
+tweet_judge.to_csv("judge_tweets.csv")
 
-print(tweet_collection)
+tweet_volpe = sentiment_model(volpe_search, max_search)
+tweet_volpe.to_csv("volpe_tweets.csv")
 
-tweet_collection.to_csv("player_tweets.csv")
+tweet_donaldson = sentiment_model(donaldson_search, max_search)
+tweet_donaldson.to_csv("donaldson_tweets.csv")
+
+tweet_ikf = sentiment_model(ikf_search, max_search)
+tweet_ikf.to_csv("ikf_tweets.csv")
+
+tweet_hicks = sentiment_model(hicks_search, max_search)
+tweet_hicks.to_csv("hicks_tweets.csv")
