4e529beebdb99d3d84059de2ec7d39f283d3d9b9
Liam McCarrick <wrmccarrick@gmail.com>
2023-04-18 22:59:25 -0400

    created scraper function

--- data_collection.py
+++ data_collection.py
@@ -1,1 +1,14 @@
-print("This is where the data will be collected")
+# import packages
+import pandas as pd
+import numpy as np
+from twitter_scraper import twitter_scraper
+
+# define parameters
+adv_search = '"yankees" "volpe" OR "donaldson" OR "judge" OR "falefa" OR "ikf" OR "hicks" lang:en until:2023-04-18 since:2023-04-04'
+test_search = '"iron man" OR "tony stark"'
+max_search = 50000
+test_max = 10
+
+player_tweets = twitter_scraper(adv_search, test_max)
+
+print(player_tweets)
--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,63 +1,26 @@
 # import packages
-import tweepy
+import itertools
 import pandas as pd
-import numpy as np
-from secrets_key import consumer_key, consumer_secret, access_token, access_secret
+import snscrape.modules.twitter as sntwitter
 
-# define variables
-tweetsPerQry = 100
-maxTweets = 1500
-keyword = "yankees"
 
-# authenticate twitter project
-authentication = tweepy.OAuthHandler(consumer_key, consumer_secret)
-authentication.set_access_token(access_token, access_secret)
-api = tweepy.API(
-    authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True
-)
-maxId = -1
-tweetCount = 0
+# create scraper function
+def twitter_scraper(query, limit):
+    # create tweets list and search limit
+    scraped_tweets = sntwitter.TwitterSearchScraper(query).get_items()
 
-meta_list = []
+    # slicing the generator to keep only the first n tweets
+    sliced_scraped_tweets = itertools.islice(scraped_tweets, limit)
 
-# scrape twitter for tweets with keyword
-while tweetCount < maxTweets:
-    if maxId <= 0:
-        newTweets = api.search(
-            q=keyword, count=tweetsPerQry, result_type="recent", tweet_mode="extended"
-        )
-    else:
-        newTweets = api.search(
-            q=keyword,
-            count=tweetsPerQry,
-            max_id=str(maxId - 1),
-            result_type="recent",
-            tweet_mode="extended",
-        )
+    df = pd.DataFrame(sliced_scraped_tweets)[
+        [
+            "date",
+            "renderedContent",
+            "retweetCount",
+            "likeCount",
+            "quoteCount",
+            "viewCount",
+        ]
+    ]
 
-    if not newTweets:
-        print("Done")
-        break
-
-    # store pulled tweets
-    for tweet in newTweets:
-        username = tweet.user.name
-        created_at = str(tweet.created_at)
-        tweet_text = tweet.full_text
-        tweet_text_sent = tweet.full_text
-        retweet_count = tweet.retweet_count
-        fav_count = tweet.favorite_count
-        media_source = tweet.source
-        data_dict = {
-            "username": username,
-            "created_at": created_at,
-            "tweet_text": tweet_text,
-            "retweet_count": retweet_count,
-            "fav_count": fav_count,
-            "media_source": media_source,
-        }
-        meta_list.append(data_dict)
-    tweetCount += len(newTweets)
-    maxId = newTweets[-1].id
-
-print(meta_list)
+    return df
