8eeef30dcdd0a2dfe6d8c5528f189bfef4e38ef3
Liam McCarrick <wrmccarrick@gmail.com>
2023-04-19 01:13:30 -0400

    created sentiment analysis function

--- data_collection.py
+++ data_collection.py
@@ -1,14 +1,15 @@
 # import packages
 import pandas as pd
-import numpy as np
-from twitter_scraper import twitter_scraper
+from sentiment_analysis import sentiment_model
 
 # define parameters
 adv_search = '"yankees" "volpe" OR "donaldson" OR "judge" OR "falefa" OR "ikf" OR "hicks" lang:en until:2023-04-18 since:2023-04-04'
-test_search = '"iron man" OR "tony stark"'
+test_search = '"yankees" "hicks" lang:en until:2023-04-18 since:2023-04-04'
 max_search = 50000
 test_max = 10
 
-player_tweets = twitter_scraper(adv_search, test_max)
+tweet_collection = sentiment_model(test_search, test_max)
 
-print(player_tweets)
+print(tweet_collection)
+
+tweet_collection.to_csv("player_tweets.csv")
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,1 +1,34 @@
-print("this is where the sentiment analysis will be")
+# import packages
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from twitter_scraper import twitter_scraper
+
+# create sentiment analysis object
+analyzer = SentimentIntensityAnalyzer()
+
+
+def sentiment_model(query, limit):
+    # gather twitter data
+    player_tweets = twitter_scraper(query, limit)
+
+    # provide sentiment score
+    def score_output(output_dict):
+        sent_score = "neutral"
+
+        if output_dict["compound"] >= 0.05:
+            sent_score = "positive"
+
+        elif output_dict["compound"] <= -0.05:
+            sent_score = "negative"
+
+        return sent_score
+
+    def predict_sentiment(tweet_content):
+        sent_score = analyzer.polarity_scores(tweet_content)
+        return score_output(sent_score)
+
+    player_tweets["Sentiment Score"] = player_tweets["renderedContent"].apply(
+        predict_sentiment
+    )
+
+    return player_tweets
